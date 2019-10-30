e2089ea30b2b8bc556228a8925a99d3435dde735
seanwayland <seanwayland@gmail.com>
2019-10-30 11:14:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import { Provider } from 'react-redux';
+// import AutoComplete from '../components/PostAutoComplete'
+// import {store} from '../store'
+
+export default () => {
+    return (
+        <div>
+            Hello  Shawn
+        </div>
+    )
+}
