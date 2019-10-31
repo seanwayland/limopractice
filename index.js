630839519befb7f4cdbb67a62d8c4dc7c23ef41e
seanwayland <seanwayland@gmail.com>
2019-10-30 23:04:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,51 +25,71 @@ import Button from '@material-ui/core/Button';
 
 function renderRow(props) {
     const { index, style } = props;
-    
-  
+
+
     return (
-      <ListItem button style={style} key={index}>
-        <ListItemText primary={`Item ${index + 1}`} />
-      </ListItem>
+        <ListItem button style={style} key={index}>
+            <ListItemText primary={`Item ${index + 1}`} />
+        </ListItem>
     );
-  }
+}
 
 
-  renderRow.propTypes = {
+renderRow.propTypes = {
     index: PropTypes.number.isRequired,
     style: PropTypes.object.isRequired,
-  };
+};
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            /* .then(json => console.log(json)) */
+
+            .then(data => this.setState({ data }));
+
 
     }
 
+
     render() {
-        
+
+        {console.log(this.state)}
+
+        const limo = this.state.data ;
+
+        {console.log(limo)}
+
+
+
         return (
             <div>
                 {/*<Header/>*/}
-                <table>
-                    <tbody>
-                      
-                    { /* this.state.data.map((person, i) => <TableRow key = {i}
-                                                                  data = {person} />)     */}  
-                    </tbody>
-                </table>
-
-                        <TextField
-          placeholder="Type in Content"
-          label="Type in Content">  </TextField>
-
-          <LimoList/>
-         
 
 
-                  <Button variant="contained" color="primary">
-        Welcome Material UI
-      </Button> 
+                {console.log(limo)}
+
+
+
+                <TextField
+                    placeholder="Type in Content"
+                    label="Type in Content">  </TextField>
+
+                {/*} <LimoList/> */}
+
+
+
+                <Button variant="contained" color="primary">
+                    Welcome Material UI
+                </Button>
 
 
             </div>
@@ -90,12 +110,12 @@ class Stuff extends React.Component {
     render() {
         return (
             <div>
-            <h1>Mortgage Master</h1>
-            <h2>loves me </h2>
-        <p>This is it!!!</p>
+                <h1>Mortgage Master</h1>
+                <h2>loves me </h2>
+                <p>This is it!!!</p>
 
 
-        </div>
+            </div>
         );
     }
 }
@@ -116,58 +136,68 @@ class LimoList extends React.Component {
     constructor() {
         super();
 
-    this.state = {
-        data: null,
-      };
     }
 
-    componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-         /* .then(json => console.log(json)) */
-     
-        .then(data => this.setState({ data }));
-           
-        
-    }
 
- 
     render() {
-        
-      return (
-        <div>
-   
-        <List  subheader={<li />}>
-      {[0, 1, 2, 3, 4].map(sectionId => (
-        <li key={`section-${sectionId}`} >
-          <ul >
-            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-            {this.state.data.map(item => (
-              <ListItem key={`item-${sectionId}-${item.id}`}>
-                <ListItemText primary={`Item ${item.body}`} />
-              </ListItem>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </List>
- 
-        
 
-              {/*}  <ListItemText primary={`Item ${this.state.data}`} /> */}
-      
-        
-    
-        
+
+
+        return (
+            <div>
+
+
+                {/*
+                <List  subheader={<li />}>
+                    {[0, 1, 2, 3, 4].map(sectionId => (
+                        <li key={`section-${sectionId}`} >
+                            <ul >
+                                <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+                                {this.state.data.map(item => (
+                                    <ListItem key={`item-${sectionId}-${item.id}`}>
+                                        <ListItemText primary={`Item ${item.body}`} />
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </List>
+                */}
+
+                <List subheader={<li />}>
+
+                    {[0, 1, 2, 3, 4].map(sectionId => (
+                        <li key={`section-${sectionId}`} >
+                            <ul>
+                                <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+                                {this.state.data.map(item => (
+                                   /*  {[0, 1, 2].map(item => ( */
+                                    <ListItem key={`item ${item.id}`}>
+                                        <ListItemText primary={`Item ${item.body}`} />
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </List>
+
+
+
+                {/*}  <ListItemText primary={`Item ${this.state.data}`} /> */}
+
+
+
+
+                }
+            </div>
+        );
     }
-        </div>
-      );
-    }
-  }
+}
 
 
 
 App.defaultProps = {
+
     headerProp: "Header from props...",
     contentProp:"Content from props...",
     propArray: [1, 2, 3, 4, 5],
@@ -175,7 +205,7 @@ App.defaultProps = {
     propFunc: function (e) {
         return e
     },
-    
+
     propNumber: 1,
     propString: "Lets Do this...",
 
