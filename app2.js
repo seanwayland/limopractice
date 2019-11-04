2299c90774b6adc35903a52ffc9884867a8ad501
seanwayland <seanwayland@gmail.com>
2019-11-04 17:29:40 -0500

    working

--- app2.js
+++ app2.js
@@ -1,50 +1,34 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   data: null,
-    //   isLoaded: false,
-    //   itemSelected: false,
-    //   inputVal: "",
-    //   options: [],
-    //   selectedOption: ""
-    // };
-    this.state = {list: [], itemSelected: false, selectedItem: null}
+ 
+    this.state = {list: [], itemSelected: false, selectedItem: {}, biglist: []}
 
     this.updateState = this.updateState.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleSelectedChange = this.handleSelectedChange.bind(this);
   }
 
   updateState(e) {
     this.setState({ selectedOption: e.target.textContent, itemSelected: true });
     console.log(e.target);
-    // eventually I want to render a DIV with data from the selected value
+
   }
 
   handleChange(e) {
-    console.log(e.target.value);
-    console.log(this.state);
-    // fetch('http://jsonplaceholder.typicode.com/posts')
-    // .then(response => response.json())
-    // /* .then(json => console.log(json)) */
 
-    // .then(data => this.setState({list: data}));
-   var filtered = this.state.list.filter(it => it.body.includes(e.target.value));
+
+    //console.log(e.target.value);
+    console.log(this.state);
+   var filtered = this.state.biglist.filter(it => it.body.includes(e.target.value));
    console.log(filtered);
    this.setState({list: filtered})
    console.log(this.state.list)
@@ -56,13 +40,16 @@ class App extends React.Component {
       console.log(id);
       console.log(this.state.list);
       //var filteredClick = this.state.list.filter(it => it.body.includes(e.target));
-      var filteredClick = this.state.list.find(item => item.id === id)
+      var filteredClick = this.state.biglist.find(item => item.id === id)
 
       console.log(filteredClick);
-      this.setState({itemSelected: true})
+      this.setState({itemSelected: true, selectedItem: filteredClick});
       
-       
+      
+  }
 
+  handleSelectedChange(e){
+    this.setState({itemSelected: false})
   }
 
 
@@ -72,7 +59,9 @@ class App extends React.Component {
         .then(response => response.json())
         /* .then(json => console.log(json)) */
 
-        .then(data => this.setState({list: data}));
+        .then(data => this.setState({biglist: data}));
+
+        console.log(this.state.biglist)
 
 
 }
@@ -84,9 +73,35 @@ class App extends React.Component {
    
     if (itemSelected){
 
+        console.log(this.state.selectedItem);
+
         return (
 
-         <div> item selected </div>
+         <div> 
+
+                         <TextField
+       
+       label="Type In Content"
+       id="limoSelected"
+       //onChange = {this.fetchData}
+      // value=""
+       autoSelect={true}
+       onChange = {this.handleSelectedChange}
+       margin="normal"
+       variant="outlined"
+       fullWidth
+   
+     />
+             
+         <p>userId: {this.state.selectedItem.userId}</p>
+         <p>Id: {this.state.selectedItem.Id}</p>
+         <p>title: {this.state.selectedItem.title} </p>
+         <p>body: {this.state.selectedItem.body}</p>
+       
+             
+             
+             
+              </div>
         )
     }
 
